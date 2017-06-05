6aafa6de5b63507f7a90bc9f36139c5722d3191a
MoritaSuguru <morisug4645@gmail.com>
2017-06-05 10:06:09 +0900

    【MVC環境の導入】
    ・MVCそれぞれのスクリプトファイルを追加しました。
    ・MVCそれぞれの名前空間を追加しました。

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -22,15 +22,26 @@ namespace ToyBox {
 
 		AudioNS.AudioManager m_audioManager;
 
-		// Use this for initialization
+		Logic.LogicManager m_logicManager;
+		View.ViewManager m_viewManager;
+
+		// Use this for initializationm
 		void Start() {
 			m_audioManager = AudioNS.AudioManager.Instance;
+			
+			m_logicManager = new Logic.LogicManager();
+			m_viewManager = new View.ViewManager();
+
 			m_audioManager.Initialize();
+
+			m_logicManager.Initialize();
+			m_viewManager.Initialize(m_logicManager);
 		}
 
 		// Update is called once per frame
 		void Update() {
 			m_audioManager.UpdateByFrame();
+			
 		}
 	}
 }
