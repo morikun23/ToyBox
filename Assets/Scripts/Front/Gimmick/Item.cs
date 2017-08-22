142dcf03c21fba8a1207f51593d1de3f162b0005
morita.suguru <morisug4645@gmail.com>
2017-08-23 01:08:58 +0900

    【ギミック作成】

---
+++ Assets/Scripts/Front/Gimmick/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public class Item : MonoBehaviour {
+
+		//掴まれている状態か
+		protected bool isGrasped;
+
+		public void OnEnterTrigger2D(Collider2D arg_collider) {
+
+		}
+	}
+}
---
+++ Assets/Scripts/Front/Gimmick/PortableItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public class PortableItem : MonoBehaviour {
+
+		
+	}
+}
