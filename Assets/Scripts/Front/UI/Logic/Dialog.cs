e09b20f49c833ae533b25c935b825d6eab5a691f
DA\nwuser <noppo.kappa@gmail.com>
2017-09-08 09:38:01 +0900

    【UI】ダイアログ作成(ソースのリファクタリング必要)

---
+++ Assets/Scripts/Front/UI/Logic/Dialog.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    
+
+    public Dialog() { }
+
+    public Dialog(Transform arg_dialog) { m_dialogBuf = arg_dialog; }
+
+    //自身のTransform
+    private Transform m_dialogBuf;
+
+
+    public Transform m_dialog
+    {
+        get
+        {       if (m_dialogBuf == null)
+            {
+                m_dialogBuf = this.transform;
+            }
+            return m_dialogBuf;
+        }
+    }
+
+    public void SetDialog(Transform arg_dialog)
+    {
+        m_dialogBuf = arg_dialog;
+    }
+
+
+    void Initialize()
+    {
+        
+    }
+    void UpdateByFrame()
+    {
+        if (transform.localScale.x <= 2.5f)
+        {
+            transform.localScale += new Vector3(0.05f, 0, 0);
+        }
+    }
+
+    void Update()
+    {
+        UpdateByFrame();
+    }
+
+
+}
+
+
