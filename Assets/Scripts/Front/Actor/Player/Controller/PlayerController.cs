5f505a4564e2c1dc246e5adb32438a65542bc9c2
morita.suguru <morisug4645@gmail.com>
2017-09-09 21:06:11 +0900

    【プレイヤー】Controller作成

---
+++ Assets/Scripts/Front/Actor/Player/Controller/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public class PlayerController : ObjectBase {
+
+		public Player m_player { get; private set; }
+
+		public Arm m_arm { get; private set; }
+
+		public void Initialize() {
+			m_player = FindObjectOfType<Player>();
+			if (!m_player) { PlayerGenerate(); }
+		}
+
+		public void UpdateByFrame() {
+
+		}
+
+		private void PlayerGenerate() {
+			Instantiate(Resources.Load<Player>("Actor/Player/Player") , m_transform);
+		}
+	}
+}
