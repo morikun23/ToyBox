372dc09776cf674a73d9f7c68b6c1ed0c5f66986
morita.suguru <morisug4645@gmail.com>
2017-09-18 21:25:31 +0900

    【ハンド】描画処理を適用

--- Assets/Scripts/Front/Actor/MagicHand/Logic/Hand.cs
+++ Assets/Scripts/Front/Actor/MagicHand/Logic/Hand.cs
@@ -11,14 +11,15 @@ namespace ToyBox {
 	[System.Serializable]
 	public class Hand : ObjectBase {
 
-		public Queue<IHandCommand> m_task { get; private set; }
+		private HandViewer m_viewer;
 
 		/// <summary>
 		/// 初期化
 		/// </summary>
 		/// <param name="arg_arm"></param>
 		public void Initialize(Arm arg_arm) {
-			m_task = new Queue<IHandCommand>();
+			m_viewer = GetComponentInChildren<HandViewer>();
+			m_viewer.Initialize(this);
 		}
 
 		/// <summary>
@@ -26,17 +27,13 @@ namespace ToyBox {
 		/// </summary>
 		/// <param name="arg_arm"></param>
 		public void UpdateByFrame(Arm arg_arm) {
-			
-		}
 
-		/// <summary>
-		/// アームの向きに合わせて角度を調整する
-		/// </summary>
-		/// <param name="arg_angle">角度</param>
-		public void SetRotation(float arg_angle) {
-			m_transform.eulerAngles = Vector3.forward * arg_angle;
-		}
+			m_transform.position = arg_arm.GetTopPosition();
 
+			m_viewer.UpdateByFrame(this);
+			m_viewer.SetRotation(arg_arm.m_currentAngle);
+		}
+		
 		public void SetGrabbable() {
 
 		}
