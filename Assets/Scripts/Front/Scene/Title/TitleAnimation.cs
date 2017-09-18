4c31e8328998a61d3ac4d9572bcffa9f25fed070
morita.suguru <morisug4645@gmail.com>
2017-09-19 01:42:02 +0900

    【タイトルシーン】※本番で使用するファイルは「SandBox」内に入れないでください。

--- Assets/Scripts/Front/Scene/Title/TitleAnimation.cs
+++ Assets/Scripts/Front/Scene/Title/TitleAnimation.cs
@@ -6,26 +6,26 @@ namespace ToyBox.Title
 {
     public class TitleAnimation : MonoBehaviour
     {
-        Vector3 m_move = new Vector3(0.05f, 0, 0);
-        Vector3 m_reStart = new Vector3(-15f, -3.85f, 0);
-        Animator m_animator;
-        SpriteRenderer m_renderer;
+        Vector3 Move = new Vector3(0.05f, 0, 0);
+        Vector3 Restart = new Vector3(-15f, -3.85f, 0);
+        Animator animator;
+        SpriteRenderer renderer;
 
-        [SerializeField]
-        GameObject m_spComment;
-
-        TitleComment m_titleComment;
+        GameObject sp_Comment;
+        TitleComment titleComment;
 
 
-        bool m_stop = true;
+        bool stop = true;
 
         // Use this for initialization
         void Start()
         {
-            m_animator = GetComponent<Animator>();
-            m_renderer = GetComponent<SpriteRenderer>();
-            m_titleComment = m_spComment.GetComponent<TitleComment>();
+            animator = GetComponent<Animator>();
+            renderer = GetComponent<SpriteRenderer>();
+            sp_Comment = GameObject.Find("SP_Comment");
+            //titleComment = sp_Comment.GetComponent<TitleComment>();
             StartCoroutine("Wait");
+            //titleComment.Initialize();
             
         }
 
@@ -35,30 +35,29 @@ namespace ToyBox.Title
             while (true)
             {
 
-                if (transform.position.x >= 5.5f && m_stop)
+                if (transform.position.x >= 5.5f && stop)
                 {
-                    m_renderer.flipX = false;
-                    m_animator.SetBool("RunFlag", false);
-                    m_animator.SetBool("IdleFlag", true);
-                    yield return new WaitForSeconds(3.0f);
-                    m_titleComment.Spawn();
-                    yield return new WaitForSeconds(1.0f);
-                    m_titleComment.Delete();
+                    renderer.flipX = false;
+                    animator.SetBool("RunFlag", false);
+                    animator.SetBool("IdleFlag", true);
+                    //titleComment.Spawn();
+                    yield return new WaitForSeconds(4.0f);
+                    //titleComment.Delete();
 
-                    m_stop = false;
+                    stop = false;
                 }
                 else
                 {
-                    m_renderer.flipX = true;
-                    m_animator.SetBool("RunFlag", true);
-                    m_animator.SetBool("IdleFlag", false);
-                    transform.position += m_move;
+                    renderer.flipX = true;
+                    animator.SetBool("RunFlag", true);
+                    animator.SetBool("IdleFlag", false);
+                    transform.position += Move;
                 }
 
                 if (transform.position.x >= 10f)
                 {
-                    transform.position = m_reStart;
-                    m_stop = true;
+                    transform.position = Restart;
+                    stop = true;
                 }
 
 
