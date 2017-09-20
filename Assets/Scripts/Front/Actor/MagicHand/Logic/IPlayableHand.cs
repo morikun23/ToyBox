4ecc25286b44b28dacd00696d63520f021bce228
morita.suguru <morisug4645@gmail.com>
2017-09-20 16:04:21 +0900

    【ハンド】ハンドの入力クラスとのインターフェイスを用意しました

---
+++ Assets/Scripts/Front/Actor/MagicHand/Logic/IPlayableHand.cs
@@ -0,0 +1,22 @@
+﻿//担当
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public interface IPlayableHand {
+
+		/// <summary>
+		/// タップしたアイテムをバッファとして持たせる
+		/// 伸ばす前にこれを実行してください
+		/// </summary>
+		/// <param name="arg_item"></param>
+		void SetItemBuffer(Item arg_item);
+
+		/// <summary>
+		/// 所持しているアイテムを離す
+		/// </summary>
+		void Release();
+	}
+}
