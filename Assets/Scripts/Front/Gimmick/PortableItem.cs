2fe034f4e7610d2c15c285958fd2b3c75951beeb
MoritaSuguru <morisug4645@gmail.com>
2017-09-23 05:02:13 +0900

    【持ち運び可アイテム】持ち運び処理を実装

--- Assets/Scripts/Front/Gimmick/PortableItem.cs
+++ Assets/Scripts/Front/Gimmick/PortableItem.cs
@@ -3,8 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace ToyBox {
-	public class PortableItem : MonoBehaviour {
+	public class PortableItem : Item {
 
-		
+		public override void OnGraspedEnter(PlayerComponent arg_player) {
+			arg_player.Arm.m_shorten = true;
+		}
+
+		public override void OnGraspedStay(PlayerComponent arg_player) {
+			this.m_transform.position = arg_player.Arm.GetTopPosition();
+			if (arg_player.m_currentState.GetType() != typeof(PlayerReachState)) {
+				arg_player.m_rigidbody.isKinematic = false;
+			}
+		}
+
+		public override void OnGraspedExit(PlayerComponent arg_player) {
+			
+			arg_player.m_inputHandle.m_reach = false;
+		}
 	}
 }
