00c8d86d8999f3147733d95ba4b5135f6128e6f1
MoritaSuguru <morisug4645@gmail.com>
2017-09-23 05:01:22 +0900

    【移動グリップ】リファクタリング

--- Assets/Scripts/Front/Gimmick/MovableGlip.cs
+++ Assets/Scripts/Front/Gimmick/MovableGlip.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 namespace ToyBox {
 	public class MovableGlip : FixedItem {
 
+		private Vector3 m_direction;
+
 		public override void OnGraspedEnter(PlayerComponent arg_player) {
 			base.OnGraspedEnter(arg_player);
 			arg_player.Arm.m_shorten = true;
+			m_direction = ((Vector2)arg_player.Arm.m_transform.position - arg_player.Arm.m_targetPosition).normalized;
+
 		}
 
 		public override void OnGraspedStay(PlayerComponent arg_player) {
 			ArmComponent arm = arg_player.Arm;
 
-			Vector3 direction = ((Vector2)arm.m_transform.position - arm.m_targetPosition).normalized;
-
 			if (arm.m_lengthBuf.Count > 0) {
-				arm.m_transform.position = m_transform.position + (direction * arm.m_lengthBuf.Peek());
+				arm.m_transform.position = m_transform.position + (m_direction * arm.m_lengthBuf.Peek());
 			}
 			else {
 				arm.m_transform.position = m_transform.position;
