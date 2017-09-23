45f3741040a069a0d9cd595b955a52b1b69d0102
morita.suguru <morisug4645@gmail.com>
2017-09-23 21:09:40 +0900

    【ステージ】ステージ内のスタート地点、チェックポイント地点を実装

---
+++ Assets/Scripts/Front/Stage/CheckPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public class CheckPoint : StartPoint {
+
+		public override void Initialize(PlayerComponent arg_player) {
+			base.Initialize(arg_player);
+		}
+
+		public override void Generate(PlayerComponent arg_player) {
+			base.Generate(arg_player);
+		}
+	}
+}
---
+++ Assets/Scripts/Front/Stage/StartPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public class StartPoint : MonoBehaviour {
+
+		public virtual void Initialize(PlayerComponent arg_player) {
+
+		}
+
+		public virtual void Generate(PlayerComponent arg_player) {
+			arg_player.m_transform.position = this.transform.position;
+		}
+	}
+}
