bb7b2ffcdaae28af7e4d9b039a300aa2f969912d
morita.suguru <morisug4645@gmail.com>
2017-09-25 02:41:28 +0900

    【チェックポイント】スタート地点、ゴールの判定を実装

--- Assets/Scripts/Front/Scene/Main/MainScene.cs
+++ Assets/Scripts/Front/Scene/Main/MainScene.cs
@@ -6,18 +6,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace ToyBox.Main {
-	public class MainScene : Scene {
+	public class MainScene : ToyBox.Scene {
+
+		private ActorManager m_actorManager;
+
+		private Stage m_stage;
 
 		public override IEnumerator OnEnter() {
+			m_actorManager = FindObjectOfType<ActorManager>();
+			m_actorManager.Initialize();
+
+			//ステージの生成
+			m_stage = FindObjectOfType<Stage>();
+			m_stage.Initialize();
+			m_stage.PlayerGenerate(m_actorManager.m_player);
 			AppManager.Instance.m_fade.StartFade(new FadeIn() , Color.black , 0.5f);
 			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+			
 		}
 
 		public override IEnumerator OnUpdate() {
 			while (true) {
-				if (Input.GetKeyDown(KeyCode.Space)) {
+				m_actorManager.UpdateByFrame();
+				m_stage.UpdateByFrame();
+
+				if (m_stage.DoesPlayerReachGoal()) {
 					break;
 				}
 				yield return null;
@@ -27,7 +43,7 @@ namespace ToyBox.Main {
 		public override IEnumerator OnExit() {
 			AppManager.Instance.m_fade.StartFade(new FadeOut() , Color.black , 0.5f);
 			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
-			UnityEngine.SceneManagement.SceneManager.LoadScene("StartUp");
+			SceneManager.LoadScene("StartUp");
 			yield return null;
 
 		}
