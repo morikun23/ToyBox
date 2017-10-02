8322d3d86bac6e3b65880d1cb732ecabb65942a2
DA\nwuser <noppo.kappa@gmail.com>
2017-10-02 16:19:52 +0900

    【UI】ベルトコンベア実装

---
+++ Assets/Scripts/SandBox/Kaihatsu/BeltConveyor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltConveyor : MonoBehaviour {
+
+
+    [SerializeField]
+    private float m_movePower= 100.0f;
+
+    
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+           
+        var body = other.gameObject.GetComponent<Rigidbody2D>();
+        if (body != null)
+        {
+            Vector3 addPower = transform.position * m_movePower;
+            body.AddForce(addPower, ForceMode2D.Force);
+
+            //body.MovePosition(other.transform.position + add);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        var body = other.gameObject.GetComponent<Rigidbody2D>();
+        if(body != null)
+        {
+            Vector3 addPower = transform.position * m_movePower;
+            body.AddForce(addPower, ForceMode2D.Force);
+        }
+    }
+    
+}
