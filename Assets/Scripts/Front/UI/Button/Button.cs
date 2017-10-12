a9e162704c9da197c85883b9ad3f6685f1b71b8c
morita.suguru <morisug4645@gmail.com>
2017-10-13 03:52:19 +0900

    【ボタン】プレイヤーと結びつけるために別バージョンを新規作成（急遽なので、今後Revertするかも）

---
+++ Assets/Scripts/Front/UI/Button/Button.cs
@@ -0,0 +1,57 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public class Button : ToyBox.MobileInput {
+
+		protected Playable m_playable;
+
+		protected Animator m_animator;
+
+		private bool m_isPressing;
+
+#if UNITY_EDITOR && DEVELOP
+		[SerializeField]
+		protected KeyCode m_key;
+#endif
+
+		public virtual void Initialize() {
+			m_isPressing = false;
+			m_animator = GetComponent<Animator>();
+		}
+
+		public virtual void UpdateByFrame() {
+			if (m_isPressing) { this.OnPress(); }
+		}
+
+		public sealed override void Started() {
+			base.Started();
+			m_isPressing = true;
+			this.OnDown();
+		}
+
+		public sealed override void TouchEnd() {
+			base.TouchEnd();
+			m_isPressing = false;
+			this.OnUp();
+			Debug.Log("S");
+		}
+
+		public virtual void OnDown() {
+			m_animator.SetBool("Press" , true);
+		}
+
+		public virtual void OnPress() {
+
+		}
+
+		public virtual void OnUp() {
+			m_animator.SetBool("Press" , false);
+		}
+		
+		public void SetControlTarget(Playable arg_playable) {
+			m_playable = arg_playable;
+		}
+	}
+}
---
+++ Assets/Scripts/Front/UI/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace ToyBox {
+	public class InputManager : MonoBehaviour {
+
+		//ボタン
+		private List<Button> m_buttons;
+
+		//操作キャラ
+		private Playable m_playable;
+
+		// Use this for initialization
+		void Start() {
+
+			m_playable = FindObjectOfType<Playable>();
+
+			m_buttons = FindObjectsOfType<Button>().ToList();
+			foreach(Button button in m_buttons) {
+				button.Initialize();
+				button.SetControlTarget(m_playable);
+			}
+		}
+
+		// Update is called once per frame
+		void Update() {
+			foreach (Button button in m_buttons) {
+				button.UpdateByFrame();
+			}
+		}
+	}
+}
