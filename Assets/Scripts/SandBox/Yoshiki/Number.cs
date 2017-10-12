1914958bda28d7ac67cd824bd2e9bb4e0a93d40d
DA\nwuser <ysk1033738432@gmail.com>
2017-10-12 14:16:35 +0900

    【順番に通過する】当たり判定調整中です

---
+++ Assets/Scripts/SandBox/Yoshiki/Number.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox
+{
+    public class Number : MonoBehaviour
+    {
+        public bool hitNumber = false;
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            hitNumber = true;
+        }
+    }
+}
---
+++ Assets/Scripts/SandBox/Yoshiki/NumbersOrder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ToyBox
+{
+    public class NumbersOrder : MonoBehaviour
+    {
+
+        [SerializeField]
+        GameObject [] m_number;
+        Number m_check;
+
+        int m_nowCount = 0;
+        int m_nextCount = 0;
+
+        void Start()
+        {
+            Initialize();
+        }
+        void Update()
+        {
+            UpdateByFrame();
+        }
+        
+        public void Initialize()
+        {
+            m_check = m_number[m_nowCount].GetComponent<Number>();    
+        }
+
+        public void UpdateByFrame()
+        { 
+            if (m_number[m_nowCount] == null)
+            {
+                
+            }
+            else
+            {
+                if (m_number[m_nowCount])
+                {
+
+                }
+            }
+
+
+
+        }
+    }
+}
