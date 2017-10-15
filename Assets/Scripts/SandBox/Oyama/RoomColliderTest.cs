b8b874a9e880a653c5f0c349f091003d15bbcf8e
oyama <oyameycry3024@gmail.com>
2017-10-16 00:58:12 +0900

    【Stage】ステージ２の小部屋遷移完了。それに伴ったステージ2のリファクタリング

--- Assets/Scripts/SandBox/Oyama/RoomColliderTest.cs
+++ Assets/Scripts/SandBox/Oyama/RoomColliderTest.cs
@@ -9,10 +9,20 @@ namespace ToyBox.Oyama
     {
         public int m_roomNumber;
 
+        PlayRoomManagerTest m_roomManager;
+
+        void Start()
+        {
+            m_roomManager = FindObjectOfType<PlayRoomManagerTest>();
+        }
+
         void OnTriggerEnter2D(Collider2D arg_col)
         {
-            Debug.Log("Enetr");
-            PlayRoomManagerTest.Instance.SetCurrentRoom(m_roomNumber);
+            if (arg_col.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                Debug.Log("Enetr");
+                m_roomManager.SetCurrentRoom(m_roomNumber);
+            }
         }
 
     }
