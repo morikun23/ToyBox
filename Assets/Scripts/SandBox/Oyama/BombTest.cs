8976ebfad8925e5d9c6f64ce84104239fa8ba5c4
oyama <oyameycry3024@gmail.com>
2017-10-18 17:59:46 +0900

    【Stage】チュートリアルステージほぼ設置完了。シャッターのスクリプトをちょっと改良。

--- Assets/Scripts/SandBox/Oyama/BombTest.cs
+++ Assets/Scripts/SandBox/Oyama/BombTest.cs
@@ -55,7 +55,7 @@ namespace ToyBox.Oyama
             if (m_startExplosionFlag) return;
             
             //動きが無くなった&爆破フラグが経っていないならタイム加算
-            if(m_rigid.velocity.x == 0 && m_rigid.velocity.y == 0 && m_startExplosionFlag == false)
+            if(Mathf.Abs(m_rigid.velocity.x) <= 1 && Mathf.Abs(m_rigid.velocity.y) <= 1 && m_startExplosionFlag == false)
             {
                 m_addTime += Time.deltaTime;
                 Debug.Log("加算中");
@@ -69,7 +69,8 @@ namespace ToyBox.Oyama
             //指定秒動きを止めていたら爆破フラグオン
             if(m_startExplosionTime < m_addTime && m_startExplosionFlag == false)
             {
-                Debug.Log("unko");
+                m_rigid.velocity = Vector2.zero;
+
                 m_anime.Play("Bomb");
                 m_anime.Update(0);
                 Debug.Log(m_anime.GetCurrentAnimatorStateInfo(0).length);
