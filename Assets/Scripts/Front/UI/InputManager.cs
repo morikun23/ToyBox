09030273b27ef492123be1b1cfcac8afa48a69fa
oyama <oyameycry3024@gmail.com>
2017-10-25 16:43:59 +0900

    【Stage】カメラフォーカス時にプレイヤーがとまるよう各スクリプトを変更。

--- Assets/Scripts/Front/UI/InputManager.cs
+++ Assets/Scripts/Front/UI/InputManager.cs
@@ -15,6 +15,7 @@ namespace ToyBox {
 		//カメラ
 		[SerializeField]
 		private Camera m_uiCamera;
+        
 
 		public void Initialize() {
 
@@ -26,17 +27,19 @@ namespace ToyBox {
 				button.SetControlTarget(m_playable);
 				button.SwitchCamera(m_uiCamera);
 			}
-		}
+            
+        }
 
 		public void UpdateByFrame() {
+            
 
 			if (Input.GetMouseButtonDown(0)) {
 				Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 				Vector2 worldPointUI = m_uiCamera.ScreenToWorldPoint (Input.mousePosition);
 
-				//タッチをした位置にオブジェクトがあるかどうかを判定
-				RaycastHit2D hit = Physics2D.Raycast(worldPoint , Vector2.zero , 0.1f , 1 << LayerMask.NameToLayer("Item") | 1 << LayerMask.NameToLayer("PotableItem_Ground"));
-				RaycastHit2D hitUI = Physics2D.Raycast (worldPointUI,Vector2.zero,0.1f,1 << LayerMask.NameToLayer("UI"));
+                //タッチをした位置にオブジェクトがあるかどうかを判定
+                RaycastHit2D hit = Physics2D.CircleCast(worldPoint, 0.5f, Vector2.zero , 0.1f , 1 << LayerMask.NameToLayer("Item") | 1 << LayerMask.NameToLayer("PotableItem_Ground"));
+				RaycastHit2D hitUI = Physics2D.CircleCast (worldPointUI,0.5f,Vector2.zero,0.1f,1 << LayerMask.NameToLayer("UI"));
 
 				Debug.Log (hitUI.collider);
 				if (m_playable.m_playableHand.IsGrasping() && !hitUI.collider) {
@@ -55,5 +58,27 @@ namespace ToyBox {
 				button.UpdateByFrame();
 			}
 		}
+
+
+        //入力受付関数
+        public void InputStart()
+        {
+            foreach (Button button in m_buttons)
+            {
+                button.TouchEnd();
+                button.enabled = true;
+            }
+        }
+
+        //入力禁止関数
+        public void InputStop()
+        {
+            foreach (Button button in m_buttons)
+            {
+                button.TouchEnd();
+                button.enabled = false;
+            }
+        }
+
 	}
 }
