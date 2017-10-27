862ace62309135d81c9ad378de0199a723ef7cc9
morita.suguru <morisug4645@gmail.com>
2017-10-28 01:35:49 +0900

    【Mainシーン】入場時の演出と退場時の軽い演出を実装しました。

--- Assets/Scripts/Front/Scene/Main/MainScene.cs
+++ Assets/Scripts/Front/Scene/Main/MainScene.cs
@@ -17,12 +17,21 @@ namespace ToyBox {
 
 		private InputManager m_inputManager;
 
+		#region 入場時の演出に使用する
+		[SerializeField]
+		private Animator m_doorAnimation;
+
+		private Color m_colorBuf;
+		#endregion
+
 		public override IEnumerator OnEnter() {
 			//ステージの生成（どのステージなのかを区別させたい）
 			m_stage = FindObjectOfType<Stage>();
 
 			//プレイヤーの生成
 			m_player = FindObjectOfType<Player>();
+			m_colorBuf = m_player.m_viewer.m_spriteRenderer.color;
+			m_player.m_viewer.m_spriteRenderer.color = new Color(m_colorBuf.r,m_colorBuf.g,m_colorBuf.b,0);
 
 			m_stage.Initialize(m_player);
 
@@ -32,10 +41,25 @@ namespace ToyBox {
 
 			AppManager.Instance.m_fade.StartFade(new FadeIn() , Color.black , 0.5f);
 			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+
+			#region 入場時の演出
+			m_doorAnimation.SetBool("Open",true);
+			yield return null;
+			yield return new Tsubakit.WaitForAnimation(m_doorAnimation , 0);
+
+			float alpha = 0;
 			
+			while (alpha < 1) {
+				alpha += 0.01f;
+				m_player.m_viewer.m_spriteRenderer.color = new Color(m_colorBuf.r , m_colorBuf.g , m_colorBuf.b , alpha);
+				yield return null;
+			}
+
+			#endregion
 		}
 
 		public override IEnumerator OnUpdate() {
+
 			while (true) {
 				if(m_player.GetCurrentState() != typeof(PlayerDeadState)){
 					m_inputManager.UpdateByFrame();
@@ -45,11 +69,15 @@ namespace ToyBox {
 				}
 				yield return null;
 			}
+
+			#region ゴール後の演出
+			AppManager.Instance.m_fade.StartFade(new FadeOut() , Color.white , 3.0f);
+			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+			#endregion
 		}
 
 		public override IEnumerator OnExit() {
-			AppManager.Instance.m_fade.StartFade(new FadeOut() , Color.black , 0.5f);
-			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+			
 			SceneManager.LoadScene("StartUp");
 			yield return null;
 
