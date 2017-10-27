5b2245147b2aa707b729a931a5457f71d0d1a7e8
oyama <oyameycry3024@gmail.com>
2017-10-27 10:36:34 +0900

    【Stage】チュートリアルオブジェとウィンドウを追加。

--- Assets/Scripts/Front/Gimmick/Dialog.cs
+++ Assets/Scripts/Front/Gimmick/Dialog.cs
@@ -11,27 +11,42 @@ namespace ToyBox
 
         GameObject m_signboard_obj;
 
+        float m_openNum;
+
+
         void Start()
         {
             //タグ参照(※看板には"Singboard"タグをつける必要有)
             m_signboard_obj = GameObject.FindWithTag("Signboard");
 
             m_signboard = m_signboard_obj.GetComponent<Signboard>();
+            
+
+            m_openNum = 0;
         }
 
         //ダイアログを一定値まで伸縮
         public void Extend()
         {
-            if (transform.localScale.x < 2.5f)
+            m_openNum += Time.deltaTime / 2;
+
+            if (transform.localScale.x < 0.4f)
             {
-                transform.localScale += new Vector3(0.05f, 0, 0);
+                transform.localScale += new Vector3(m_openNum, 0, 0);
+            }
+            else
+            {
+                m_openNum = 0;
+                transform.localScale = new Vector3(0.4f, 0.4f, 1);
             }
         }
 
         //ダイアログを一定値まで収縮して削除
         public void Shrink()
         {
-            transform.localScale += new Vector3(-0.05f, 0, 0);
+            m_openNum += Time.deltaTime * 2;
+
+            transform.localScale += new Vector3(-m_openNum, 0, 0);
 
             if (transform.localScale.x < 0)
             {
