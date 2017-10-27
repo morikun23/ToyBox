e6d2e1d58df6ab66911c02d35896067c548c65f0
morita.suguru <morisug4645@gmail.com>
2017-10-27 17:11:25 +0900

    【競合解決】競合してしまったファイルを再追加

---
+++ Assets/Scripts/Front/UI/Button/Change/ChosesBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+    public class ChosesBase : MonoBehaviour{
+
+        public enum YorN
+        {
+            YES = 1,
+            NO = -1,
+            DEFAULT = 0
+        }
+
+
+    }
+}
