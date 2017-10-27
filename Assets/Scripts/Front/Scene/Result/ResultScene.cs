e423370c043dc3eb9c10bedd0628c9f69b66b2de
morita.suguru <morisug4645@gmail.com>
2017-10-28 02:35:05 +0900

    【Resultシーン】リザルトシーンを実装（スプライト表示のみ）

---
+++ Assets/Scripts/Front/Scene/Result/ResultScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public class ResultScene : ToyBox.Scene {
+
+		public override IEnumerator OnEnter() {
+			AppManager.Instance.m_fade.StartFade(new FadeIn() , Color.white , 1.0f);
+			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+		}
+
+		public override IEnumerator OnUpdate() {
+
+			AppManager.Instance.m_audioManager.CreateSe("SE_Gimmick_Clear").Play();
+
+			while (true) {
+				if (Input.GetMouseButtonDown(0)) {
+					break;
+				}
+				yield return null;
+			}
+		}
+
+		public override IEnumerator OnExit() {
+
+			AppManager.Instance.m_fade.StartFade(new FadeOut() , Color.black , 2.0f);
+			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+
+			UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+		}
+	}
+}
