95d30ff487b06ed3296ea9a5be3d949647a624a1
morita.suguru <morisug4645@gmail.com>
2017-10-28 12:04:59 +0900

    【Tutorialシーン】最初の看板を読むまで操作に制限をかけました

---
+++ Assets/Scripts/Front/Scene/Tutorial/IntrodcutionSignBoard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public class IntrodcutionSignBoard : Signboard {
+
+		public GameObject m_animation;
+
+		//この看板は一度でも読まれたか（進行を制御するためのフラグ）
+		private bool m_isRead;
+
+		public override void OnGraspedEnter(PlayerComponent arg_player) {
+			base.OnGraspedEnter(arg_player);
+		}
+
+		public override void OnGraspedStay(PlayerComponent arg_player) {
+			base.OnGraspedStay(arg_player);
+		}
+
+		public override void OnGraspedExit(PlayerComponent arg_player) {
+			base.OnGraspedExit(arg_player);
+			m_isRead = true;
+		}
+
+		public bool IsRead() {
+			return m_isRead;
+		}
+	}
+}
--- Assets/Scripts/Front/Scene/Tutorial/TutorialScene.cs
+++ Assets/Scripts/Front/Scene/Tutorial/TutorialScene.cs
@@ -3,74 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace ToyBox
-{
-    public class TutorialScene : ToyBox.Scene
-    {
+namespace ToyBox {
+	public class TutorialScene : ToyBox.Scene {
 
-        private Stage m_stage;
+		private Stage m_stage;
 
-        private Player m_player;
+		private Player m_player;
 
-        private InputManager m_inputManager;
+		private InputManager m_inputManager;
 
-        [SerializeField]
-        private Animator m_doorAnimation;
+		[SerializeField]
+		private Animator m_doorAnimation;
 
-        public override IEnumerator OnEnter()
-        {
-            
-            //ステージの生成（どのステージなのかを区別させたい）
-            m_stage = FindObjectOfType<Stage>();
+		//最初に読ませる看板
+		private IntrodcutionSignBoard m_introduction;
 
-            //プレイヤーの生成
-            m_player = FindObjectOfType<Player>();
+		public override IEnumerator OnEnter() {
 
-            m_stage.Initialize(m_player);
+			//ステージの生成（どのステージなのかを区別させたい）
+			m_stage = FindObjectOfType<Stage>();
 
-            //入力環境を初期化
-            m_inputManager = GetComponent<InputManager>();
-            m_inputManager.Initialize();
+			//プレイヤーの生成
+			m_player = FindObjectOfType<Player>();
 
-            AppManager.Instance.m_fade.StartFade(new FadeIn(), Color.black, 1.0f);
-            yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+			m_stage.Initialize(m_player);
 
-            yield return new WaitForSeconds(1.5f);
-        }
+			//入力環境を初期化
+			m_inputManager = GetComponent<InputManager>();
+			m_inputManager.Initialize();
 
-        public override IEnumerator OnUpdate()
-        {
+			m_introduction = FindObjectOfType<IntrodcutionSignBoard>();
 
-            while (true)
-            {
-                if (m_player.GetCurrentState() != typeof(PlayerDeadState))
-                {
-                    m_inputManager.UpdateByFrame();
-                }
-                if (m_stage.DoesPlayerReachGoal())
-                {
-                    m_doorAnimation.SetBool("Open", true);
+			m_introduction.m_animation.SetActive(false);
 
-                    yield return null;
-                    yield return new Tsubakit.WaitForAnimation(m_doorAnimation, 0);
-                    break;
-                }
-                yield return null;
-            }
+			AppManager.Instance.m_fade.StartFade(new FadeIn() , Color.black , 1.0f);
+			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
 
-            #region ゴール後の演出
-            AppManager.Instance.m_fade.StartFade(new FadeOut(), Color.white, 3.0f);
-            yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
-            #endregion
-        }
+			
 
-        public override IEnumerator OnExit()
-        {
+			yield return new WaitForSeconds(2.0f);
+		}
 
-            SceneManager.LoadScene("Select");
-            yield return null;
+		public override IEnumerator OnUpdate() {
+			#region 最初の看板を読ませるための演出
+			m_inputManager.InputStop();
+			
+			m_introduction.m_animation.SetActive(true);
+			while (true) {
+				m_inputManager.UpdateByFrame();
 
-        }
-    }
+				if (m_introduction.IsRead()) {
+					m_introduction.m_animation.SetActive(false);
+					break;
+				}
+				yield return null;
+			}
+			
+			m_inputManager.InputStart();
 
+			#endregion
+
+			while (true) {
+				if (m_player.GetCurrentState() != typeof(PlayerDeadState)) {
+					m_inputManager.UpdateByFrame();
+				}
+				if (m_stage.DoesPlayerReachGoal()) {
+					m_doorAnimation.SetBool("Open" , true);
+
+					yield return null;
+					yield return new Tsubakit.WaitForAnimation(m_doorAnimation , 0);
+					break;
+				}
+				yield return null;
+			}
+
+			#region ゴール後の演出
+			AppManager.Instance.m_fade.StartFade(new FadeOut() , Color.white , 3.0f);
+			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+			#endregion
+		}
+
+		public override IEnumerator OnExit() {
+
+			SceneManager.LoadScene("Select");
+			yield return null;
+
+		}
+	}
 }
