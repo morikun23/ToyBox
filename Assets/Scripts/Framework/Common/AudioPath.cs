407e1e143f7923ae31982adf341fdf9a543c608a
morita.suguru <morisug4645@gmail.com>
2017-11-22 16:56:04 +0900

    【フレームワーク】AudioManagerを暫定で再実装。リニューアル版の仕様はリファレンスにて

---
+++ Assets/Scripts/Framework/Common/AudioPath.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox {
+	public partial class ResourcePath {
+
+		public const string AUDIO = "Audio/";
+		public const string SE = AUDIO + "SE/";
+		public const string BGM = AUDIO + "BGM/";
+	}
+}
