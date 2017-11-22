3c0c30ca79fbd9a195a173abeaf687f885825483
DA\nwuser <noppo.kappa@gmail.com>
2017-11-22 17:04:19 +0900

    【ジャンプ台】ジャンプ台作成

---
+++ Assets/Scripts/Front/Gimmick/JumpRamp/Jumpramp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox
+{
+    public class Jumpramp : MonoBehaviour
+    {
+        [Range(5,12)]
+        public float m_jumpPower;
+
+        Animator m_anim;
+
+        void Start()
+        {
+            m_anim = GetComponent<Animator>();
+        }
+
+        //プレイヤがジャンプ台に乗ったとき
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            GameObject col_player;
+            col_player = col.gameObject;
+
+            ContactPoint2D contact = col.contacts[0];
+
+            //プレイヤが上から台にに乗ったとき大ジャンプ
+            if (contact.normal.y <= -1)
+            {
+                //アニメーションを遷移
+                m_anim.SetTrigger("Jump");
+
+                //velocityでジャンプ量を制御
+                col_player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, m_jumpPower);
+
+            }
+        }
+    }
+}
