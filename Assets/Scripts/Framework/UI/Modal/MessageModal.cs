ff7baa08f78e07cdd93657352ddf8d2d826df6ee
morita.suguru <morisug4645@gmail.com>
2017-12-15 03:48:26 +0900

    【フレームワーク】簡易モーダルを暫定で実装。今後デザインの変更が必要そう。

---
+++ Assets/Scripts/Framework/UI/Modal/MessageModal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ToyBox {
+	public class MessageModal : ModalController {
+		
+		[SerializeField]
+		private Text m_text;
+
+		[HideInInspector]
+		public string m_message;
+
+		/// <summary>
+		/// 起動メソッド
+		/// </summary>
+		public override void OnActive() {
+			m_text.text = m_message;
+		}
+	}
+}
