c5d5b26ee63ff92308fd5da1ea5350292b8c8857
oyama <oyameycry3024@gmail.com>
2017-12-14 19:51:17 +0900

    【動く床】端っこでの移動時、徐々にスピードに変動が起きるよう修正

--- Assets/Scripts/Front/Gimmick/MoveBar.cs
+++ Assets/Scripts/Front/Gimmick/MoveBar.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace ToyBox.Oyama
+#if UNITY_EDITOR
+using UnityEditor;      //!< デプロイ時にEditorスクリプトが入るとエラーになるので UNITY_EDITOR で括ってね！
+#endif
+
+namespace ToyBox
 {
 
     public class MoveBar : MonoBehaviour
@@ -14,16 +18,29 @@ namespace ToyBox.Oyama
         //動くバー
         GameObject m_Bar;
 
-        //移動値
+        //最大速度
         [SerializeField]
-        float m_moveNum;
-        
-        Vector2 nowPos,targetPos;
+        [Range(0.05f,0.1f)]
+        float m_moveMaxNum;
+
+        //実際の移動値
+        float m_moveNumReal;
+
+        Vector2 nowPos, targetPos;
 
         [SerializeField]
         bool m_actionFlag = true;
 
-		private Rigidbody2D m_rigidbodyOfBar;
+        private Rigidbody2D m_rigidbodyOfBar;
+
+        //現在位置からターゲットまでの割合
+        float m_currentPosInverse;
+
+        //現在位置からターゲットまでの距離
+        float m_currentDistance;
+
+        //移動値を加算させる回数
+        int m_count = 0;
 
         // Use this for initialization
         void Start()
@@ -31,35 +48,52 @@ namespace ToyBox.Oyama
             nowPos = targetPos = Vector2.zero;
 
             //各種オブジェの参照
-            m_StartPoint = transform.Find("StartPoint").transform.position;
-            m_EndPoint = transform.Find("EndPoint").transform.position;
+            m_StartPoint = transform.Find("SP_MoveArea/StartPoint").transform.position;
+            m_EndPoint = transform.Find("SP_MoveArea/EndPoint").transform.position;
 
             m_Bar = transform.Find("Bar").gameObject;
             m_Bar.transform.position = m_StartPoint;
 
             targetPos = m_EndPoint;
 
-			//TODO:プレハブへRigidbodyを追加させる
-			if (m_rigidbodyOfBar == null) {
-				//TryCatchだと重いため、NULLチェックを行う
-				m_rigidbodyOfBar = m_Bar.gameObject.AddComponent<Rigidbody2D>();
-				m_rigidbodyOfBar.isKinematic = true;
-				m_rigidbodyOfBar.freezeRotation = true;
-			}
+            m_rigidbodyOfBar = m_Bar.GetComponent<Rigidbody2D>();
 
-			//AudioSource source = AppManager.Instance.m_audioManager.CreateBgm ("SE_BeltConveyor_move");
-			//source.Play ();
-			//source.loop = true;
+            m_moveNumReal = 0;
+            //ターゲットまでの距離を算出
+            m_currentDistance = Vector2.Distance(m_Bar.transform.position, targetPos);
+
+            //AudioSource source = AppManager.Instance.m_audioManager.CreateBgm ("SE_BeltConveyor_move");
+            //source.Play ();
+            //source.loop = true;
+
         }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
             if (!m_actionFlag) return;
 
-            nowPos = Vector2.MoveTowards(m_Bar.transform.position, targetPos, m_moveNum);
+            //ターゲットまでの距離と、現在位置の割合を算出
+            m_currentPosInverse = Mathf.InverseLerp(0, m_currentDistance, Vector2.Distance(m_Bar.transform.position, targetPos));
 
-            if (nowPos == m_StartPoint || nowPos == m_EndPoint)
+            //近いようであれば移動値を加算
+            if (m_currentPosInverse > 0.7f && m_moveNumReal != m_moveMaxNum)
+            {
+                m_moveNumReal += m_moveMaxNum / 100;
+            }
+
+            //遠いようであれば移動値を減算
+            if (m_currentPosInverse < 0.3f && m_moveNumReal > m_moveMaxNum / 100)
+            {
+                m_moveNumReal -= m_moveMaxNum / 100;
+            }
+
+            
+            //リジッドボディに加える力の量を決定
+            nowPos = Vector2.MoveTowards(m_Bar.transform.position, targetPos, m_moveNumReal);
+
+            //ターゲットまでの距離が縮まったらターゲットを入れ替える
+            if (Vector2.Distance( nowPos,targetPos) < 0.01f)
             {
                 if (targetPos == m_StartPoint)
                 {
@@ -69,22 +103,61 @@ namespace ToyBox.Oyama
                 {
                     targetPos = m_StartPoint;
                 }
-
+                
+                //移動値のリセットと新しいターゲットまでの距離を更新
+                m_moveNumReal = 0;
+                m_currentDistance = Vector2.Distance(m_Bar.transform.position, targetPos);
             }
             else
             {
-
-				m_rigidbodyOfBar.MovePosition(nowPos);
+                //バーの移動
+                m_rigidbodyOfBar.MovePosition(nowPos);
             }
         }
 
-
         //外部(スイッチとか？)から作動を開始させたい場合に呼ぶやつ
-        void Action()
+        public void Action()
         {
             m_actionFlag = true;
         }
 
-    }
+#if UNITY_EDITOR
+        /**
+         * Inspector拡張クラス
+         */
+        [CustomEditor(typeof(MoveBar))]
+        public class MoveBarEditor : Editor           //!< Editorを継承するよ！
+        {
+            MoveBar moveBar;
+            GameObject moveArea;
 
+            GameObject m_StartPointObj;
+            GameObject m_EndPointObj;
+
+            public override void OnInspectorGUI()
+            {
+                base.OnInspectorGUI();
+
+                //注意書き
+                EditorGUILayout.LabelField("※横向きで置きたい場合");
+                EditorGUILayout.LabelField("GM_MoveBarのRotationを０にした状態で押して回転してね");
+                //ボタンを追加
+                if (GUILayout.Button("Set->StartPos&EndPos"))
+                {
+
+                    this.moveBar = target as MoveBar;
+                    moveArea = moveBar.transform.Find("SP_MoveArea").gameObject;
+
+                    m_StartPointObj = moveArea.transform.Find("StartPoint").gameObject;
+                    m_EndPointObj = moveArea.transform.Find("EndPoint").gameObject;
+
+                    m_StartPointObj.transform.position = moveArea.transform.position + new Vector3(0, (moveArea.GetComponent<SpriteRenderer>().bounds.size.y / 2) - 0.3f, 0);
+                    m_EndPointObj.transform.position = moveArea.transform.position - new Vector3(0, (moveArea.GetComponent<SpriteRenderer>().bounds.size.y / 2) - 0.3f, 0);
+                }
+
+            }
+        }
+#endif
+
+    }
 }
