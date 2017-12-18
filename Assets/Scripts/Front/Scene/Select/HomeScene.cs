e0b125b86fe65a1117cddb92825b19a3c741cf4e
morita.suguru <morisug4645@gmail.com>
2017-12-19 01:49:25 +0900

    【ホーム画面】ホーム画面（現状はステージ選択画面）を実装しました

---
+++ Assets/Scripts/Front/Scene/Select/HomeScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ToyBox {
+	public class HomeScene : ToyBox.Scene {
+
+		[SerializeField]
+		HomeSceneUI m_homeSceneUI;
+
+		public override IEnumerator OnEnter() {
+
+			m_homeSceneUI.Initialize();
+
+			AppManager.Instance.m_fade.StartFade(new FadeIn() , Color.black , 1.0f);
+			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+		}
+
+		public override IEnumerator OnUpdate() {
+			yield return new WaitWhile(() => !m_homeSceneUI.IsButtonSelected());
+		}
+
+		public override IEnumerator OnExit() {
+			AppManager.Instance.m_fade.StartFade(new FadeOut() , Color.black , 1.0f);
+			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+			SceneManager.LoadScene("Main");
+		}
+	}
+}
