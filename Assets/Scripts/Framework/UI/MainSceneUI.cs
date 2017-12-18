51c6018b8846a2d4a548e6cf7f8311d7de5a7b14
DA\nwuser <ysk1033738432@gmail.com>
2017-12-18 16:33:20 +0900

    【MainUIButton】ボタン押したら動きます、ただし止まりません

--- Assets/Scripts/Framework/UI/MainSceneUI.cs
+++ Assets/Scripts/Framework/UI/MainSceneUI.cs
@@ -5,6 +5,40 @@ using UnityEngine;
 namespace ToyBox {
 	public class MainSceneUI : CommonUI {
 
-		
-	}
+        [SerializeField]
+        UIButton m_jumpButton;
+
+        [SerializeField]
+        UIButton m_leftButton;
+        [SerializeField]
+        UIButton m_rightButton;
+
+        void Start()
+        {
+            m_jumpButton.Initialize(JumpButton,"反応");
+
+            m_leftButton.Initialize(LeftButton);
+            m_rightButton.Initialize(RightButton);
+        }
+
+        void JumpButton(object arg_jump)
+        {
+            InputManager.Instance.GetPlayableCharactor().m_inputHandle.m_jump = true;
+            Debug.Log(arg_jump);
+        }
+
+        void LeftButton()
+        {
+            InputManager.Instance.GetPlayableCharactor().m_direction = ActorBase.Direction.LEFT;
+            InputManager.Instance.GetPlayableCharactor().m_inputHandle.m_run = true;
+        }
+
+        void RightButton()
+        {
+            InputManager.Instance.GetPlayableCharactor().m_direction = ActorBase.Direction.RIGHT;
+            InputManager.Instance.GetPlayableCharactor().m_inputHandle.m_run = true;
+        }
+
+
+    }
 }
