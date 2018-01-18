a3fd304849274ef90d31fed65b6531ed268f8c69
morita.suguru <morisug4645@gmail.com>
2018-01-18 15:27:22 +0900

    【プレイヤー改修】MainSceneのスクリプトを１つに統合

--- Assets/Scripts/Framework/UI/MainSceneUI.cs
+++ Assets/Scripts/Framework/UI/MainSceneUI.cs
@@ -5,68 +5,27 @@ using UnityEngine;
 namespace ToyBox {
 	public class MainSceneUI : MonoBehaviour {
 
-		[Header("Buttons")]
-		[SerializeField]
-		UIButton m_jumpButton;
 		
-		[SerializeField]
-		UIButton m_leftButton;
-
-		[SerializeField]
-		UIButton m_rightButton;
-
-		[SerializeField]
-		UIButton m_debugButton;
-
-        [SerializeField]
-        UIButton m_neoButton;
 
 
 		private Playable m_player;
 
 		public void Initialize(Playable arg_player) {
             
-			m_player = arg_player;
-			m_jumpButton.Initialize(new ButtonAction(ButtonEventTrigger.OnPress , this.OnJumpButtonDown),
-				new ButtonAction(ButtonEventTrigger.OnRelease,this.OnJumpButtonUp));
-			m_leftButton.Initialize(new ButtonAction(ButtonEventTrigger.OnPress , this.OnLeftButtonDown) ,
-				new ButtonAction(ButtonEventTrigger.OnRelease , this.OnMoveButtonUp));
-			m_rightButton.Initialize(new ButtonAction(ButtonEventTrigger.OnPress , this.OnRightButtonDown) ,
-				new ButtonAction(ButtonEventTrigger.OnRelease , this.OnMoveButtonUp));
+			//m_player = arg_player;
+			//m_jumpButton.Initialize(new ButtonAction(ButtonEventTrigger.OnPress , this.OnJumpButtonDown),
+			//	new ButtonAction(ButtonEventTrigger.OnRelease,this.OnJumpButtonUp));
+			//m_leftButton.Initialize(new ButtonAction(ButtonEventTrigger.OnPress , this.OnLeftButtonDown) ,
+			//	new ButtonAction(ButtonEventTrigger.OnRelease , this.OnMoveButtonUp));
+			//m_rightButton.Initialize(new ButtonAction(ButtonEventTrigger.OnPress , this.OnRightButtonDown) ,
+			//	new ButtonAction(ButtonEventTrigger.OnRelease , this.OnMoveButtonUp));
 
-			m_debugButton.Initialize(new ButtonAction(ButtonEventTrigger.OnRelease , this.OnDebugButton));
+			//m_debugButton.Initialize(new ButtonAction(ButtonEventTrigger.OnRelease , this.OnDebugButton));
 
-            m_neoButton.Initialize(new ButtonAction(ButtonEventTrigger.OnRelease, this.OnNeoButtonUp));
+   //         m_neoButton.Initialize(new ButtonAction(ButtonEventTrigger.OnRelease, this.OnNeoButtonUp));
 		}
 		
-		private void OnJumpButtonDown() {
-            if(m_player.m_inputHandle.m_ableJump)
-			m_player.m_inputHandle.m_jump = true;
-		}
-
-		private void OnJumpButtonUp() {
-			m_player.m_inputHandle.m_jump = false;
-		}
-
-		private void OnLeftButtonDown() {
-            if (m_player.m_inputHandle.m_ableRun)
-            {
-                m_player.m_direction = ActorBase.Direction.LEFT;
-                m_player.m_inputHandle.m_run = true;
-            }
-		}
-
-		private void OnMoveButtonUp() {
-			m_player.m_inputHandle.m_run = false;
-		}
-
-		private void OnRightButtonDown() {
-            if (m_player.m_inputHandle.m_ableRun)
-            {
-                m_player.m_direction = ActorBase.Direction.RIGHT;
-                m_player.m_inputHandle.m_run = true;
-            }
-		}
+		
 
 
 		int count = 0;
--- Assets/Scripts/Front/Scene/Main/MainScene.cs
+++ Assets/Scripts/Front/Scene/Main/MainScene.cs
@@ -6,30 +6,84 @@ using UnityEngine.SceneManagement;
 namespace ToyBox {
 	public class MainScene : ToyBox.Scene {
 
-		[SerializeField]
-		private StageFactory m_stageFactory;
+		//-------------------------------------------
+			[Header("Player")]
+		//-------------------------------------------
 
 		[SerializeField]
-		MainSceneUI m_mainSceneUI;
+		private GameObject m_playerPrefab;
 
-		private Stage m_stage;
-		
 		private Player m_player;
 
+		//-------------------------------------------
+			[Header("Buttons")]
+		//-------------------------------------------
+
+		[SerializeField]
+		UIDraggableButton m_armButton;
+
+		[SerializeField]
+		UIButton m_leftButton;
+
+		[SerializeField]
+		UIButton m_rightButton;
+
+		[SerializeField]
+		UIButton m_optionButton;
+
+		//-------------------------------------------
+			[Header("Stage")]
+		//-------------------------------------------
+		[SerializeField]
+		private StageFactory m_stageFactory;
+		
+		private Stage m_stage;
+		
 		//シーン遷移用のフラグ
-		private bool m_isAblesceneTransition;
+		private bool m_isAbleSceneTransition;
 
-		private const string PLAYER_PATH = "Contents/Player/Prefabs/BD_Player";
+		private bool m_isEnableInput = true;
 
+		private bool m_isReachMode;
+
+		[SerializeField]
+		private LineRenderer line;
+
+		//-------------------------------------------
+		//	デバッグ機能
+		//-------------------------------------------
+
+#if UNITY_EDITOR
+		[System.Serializable]
+		private class DebugInfo {
+
+			[Tooltip("デバッグモードを有効にするか")]
+			public bool m_isDebugMode;
+
+			[Tooltip("旧射出モードを利用するか")]
+			public bool m_useAnotherReachMode;
+
+
+			public bool UseAnotherReachMode {
+				get {
+					return m_isDebugMode && m_useAnotherReachMode;
+				}
+			}
+		}
+
+		[SerializeField]
+		private DebugInfo m_debugInfo;
+
+#endif
 		public override IEnumerator OnEnter() {
 
 			if (m_stageFactory == null) {
 				Debug.LogError("StageFactoryに参照できません");
 				yield break;
 			}
-			
-			uint playStageId = AppManager.Instance.user.m_temp.m_playStageId;
-			uint playRoomId = AppManager.Instance.user.m_temp.m_playRoomId;
+
+			uint playStageId = 1000;//AppManager.Instance.user.m_temp.m_playStageId;
+			uint playRoomId = 1;//AppManager.Instance.user.m_temp.m_playRoomId;
 
 			m_stage = m_stageFactory.Load(playStageId);
 
@@ -39,7 +93,7 @@ namespace ToyBox {
 			}
 
 			//プレイヤーの生成
-			m_player = Instantiate(Resources.Load<GameObject>(PLAYER_PATH)).GetComponent<Player>();
+			m_player = Instantiate(m_playerPrefab).GetComponent<Player>();
 			if (m_player == null) {
 				Debug.LogError("Playerの生成に失敗しました");
 				yield break;
@@ -47,14 +101,7 @@ namespace ToyBox {
 			m_player.gameObject.name = "Player";
 
 			m_stage.Initialize(m_player,playRoomId, this.OnPlayerGoaled);
-
-			if(m_mainSceneUI == null) {
-				Debug.LogError("UIを参照できません");
-				yield break;
-			}
-
-			//m_mainSceneUI.Initialize(m_player);
-
+			
 			AppManager.Instance.m_fade.StartFade(new FadeIn() , Color.black , 1.0f);
 
 
@@ -69,13 +116,27 @@ namespace ToyBox {
 
 			yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
 
+			m_leftButton.Initialize(
+				new ButtonAction(ButtonEventTrigger.OnPress , this.OnMoveButtonDown , (int)Player.Direction.LEFT) ,
+				new ButtonAction(ButtonEventTrigger.OnRelease , this.OnMoveButtonUp , (int)Player.Direction.LEFT)
+			);
+
+			m_rightButton.Initialize(
+				new ButtonAction(ButtonEventTrigger.OnPress , this.OnMoveButtonDown , (int)Player.Direction.RIGHT) ,
+				new ButtonAction(ButtonEventTrigger.OnRelease , this.OnMoveButtonUp , (int)Player.Direction.RIGHT)
+			);
+
+			m_armButton.Initialize(
+				new ButtonAction(ButtonEventTrigger.OnPress , this.OnArmButtonDown) ,
+				new ButtonAction(ButtonEventTrigger.OnSwipe , this.OnArmButtonStay) ,
+				new ButtonAction(ButtonEventTrigger.OnRelease , this.OnArmButtonUp)
+			);
 		}
 
 		public override IEnumerator OnUpdate() {
 			//フラグが立つまでシーン遷移を実行しない
-			yield return new WaitWhile(() => !m_isAblesceneTransition);
+			yield return new WaitWhile(() => !m_isAbleSceneTransition);
 		}
-
 		
 		public override IEnumerator OnExit() {
 			//TODO:今後リザルトシーンではなくホーム画面に移動する
@@ -89,7 +150,7 @@ namespace ToyBox {
 
 		private void OnPlayerGoaled(object arg_message) {
 			//TODO:ユーザー情報にクリアを加える
-			m_isAblesceneTransition = true;
+			m_isAbleSceneTransition = true;
 		}
 
 		/// <summary>
@@ -97,7 +158,84 @@ namespace ToyBox {
 		/// </summary>
 		public void ExitMainGame() {
 			//TODO:現在の進行度を保存する
-			m_isAblesceneTransition = true;
+			m_isAbleSceneTransition = true;
+		}
+		
+		//-------------------------------------------------------
+		//　以下、ボタン入力のコールバック
+		//-------------------------------------------------------
+
+		/// <summary>
+		/// 移動ボタンが押されたときの処理
+		/// </summary>
+		/// <param name="arg_object"></param>
+		private void OnMoveButtonDown(object arg_object) {
+			if (!m_isEnableInput) return;
+
+			int direction = (int)arg_object;
+			
+			switch (direction) {
+				case (int)Player.Direction.LEFT:	m_player.Run(Player.Direction.LEFT);	break;
+				case (int)Player.Direction.RIGHT:	m_player.Run(Player.Direction.RIGHT);	break;
+				default: Debug.LogError("ボタンからのコールバックが正しくありません");			break;
+			}
+		}
+	
+		/// <summary>
+		/// 移動ボタンが離されたときの処理
+		/// </summary>
+		/// <param name="arg_object"></param>
+		private void OnMoveButtonUp(object arg_object) {
+			if (!m_isEnableInput) return;
+
+			int direction = (int)arg_object;
+
+			switch (direction) {
+				case (int)Player.Direction.LEFT: m_player.Stop(Player.Direction.LEFT);		break;
+				case (int)Player.Direction.RIGHT: m_player.Stop(Player.Direction.RIGHT);	break;
+				default: Debug.LogError("ボタンからのコールバックが正しくありません");			break;
+			}
+		}
+
+		/// <summary>
+		/// 射出ボタンが押されたときの処理
+		/// </summary>
+		private void OnArmButtonDown() {
+			if (m_player.PlayableHand.IsGrasping) {
+				if (m_player.PlayableHand.GraspingItem.IsAbleRelease()) {
+					m_player.PlayableHand.Release();
+				}
+			}
+			else {
+				m_isReachMode = true;
+			}
+		}
+
+		/// <summary>
+		/// 射出ボタンが押されている間の処理
+		/// </summary>
+		private void OnArmButtonStay() {
+			if (!m_isReachMode) return;
+			if (m_armButton.Direction == Vector2.zero) {
+				line.enabled = false;
+			}
+			else {
+				line.enabled = true;
+				line.SetPosition(0 , m_player.PlayableArm.BottomPosition);
+				line.SetPosition(1 , m_player.PlayableArm.BottomPosition + (m_armButton.Direction * m_player.PlayableArm.Range));
+			}
+		}
+
+		/// <summary>
+		/// 射出ボタンが離されたときの処理
+		/// </summary>
+		private void OnArmButtonUp() {
+			if (!m_isReachMode) return;
+			if (m_armButton.Direction != Vector2.zero) {
+				m_player.PlayableArm.ReachOut(m_armButton.Direction);
+				//m_reachDirectionBuf = Vector2.zero;
+			}
+			line.enabled = false;
 		}
 	}
 }
