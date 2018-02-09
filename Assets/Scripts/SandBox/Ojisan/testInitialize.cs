98e6d17d7b70d609501837df2729735578c3f2cf
DA\nwuser <noppo.kappa@gmail.com>
2018-02-10 03:05:33 +0900

    【Optionモーダル】オプションの機能実装(フェードは未実装)

---
+++ Assets/Scripts/SandBox/Ojisan/testInitialize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox.Kaihatsu {
+
+    public class testInitialize : MonoBehaviour
+    {
+
+        [SerializeField]
+        UIButton unko;
+
+        [SerializeField]
+        GameObject modal;
+
+        public System.Action m_hoge;
+
+        // Use this for initialization
+        void Start()
+        {
+            unko.Initialize(new ButtonAction(ButtonEventTrigger.OnRelease, () => { UIManager.Instance.PopupOptionModal(() => { }); }));
+        }
+    }
+}
+
+
+         
